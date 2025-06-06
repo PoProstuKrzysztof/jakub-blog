61027846074d336db65cedd5affbae4e865dd509
JustChris <krzysiek.palonek@gmail.com>
2025-06-06 20:39:23 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,8 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   experimental: {
-    optimizeCss: true,
+    // Wyłączam optymalizację CSS, która powoduje błąd z critters
+    // optimizeCss: true,
   },
 }
 
