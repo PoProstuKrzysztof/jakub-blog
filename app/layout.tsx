d97b9c54ec07daf0359b9cb7ca2ce00ee3e49950
JustChris <krzysiek.palonek@gmail.com>
2025-06-16 18:57:40 +0200

    Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { Toaster } from '@/components/ui/toaster'
 import { Toaster as SonnerToaster } from '@/components/ui/sonner'
 import { AuthProvider } from '@/components/auth-provider'
+import { SiteFooter } from "@/components/site-footer"
 
 export const metadata: Metadata = {
   title: 'Jakub Inwestycje - Blog Finansowy',
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body>
         <AuthProvider>
           {children}
+          <SiteFooter />
           <Toaster />
           <SonnerToaster />
         </AuthProvider>
